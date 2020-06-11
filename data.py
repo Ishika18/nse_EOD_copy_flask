7d3794cfea1863992d436ad61c9ba0e0415cdc29
Shagun <shagunphw@gmail.com>
2020-06-10 23:52:55 -0700

    file download

--- data.py
+++ data.py
@@ -1,2 +1,7 @@
+from flask import send_file
+
+
 def download_file(given_dataframe):
     given_dataframe.to_csv('niftyfut.csv')
+    path = "niftyfut.csv"
+    return send_file(path, as_attachment=True)
